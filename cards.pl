5595ee86b05afbdc95f0bab67d1ba21f5f61fc63
JT Smith <jt@plainblack.com>
2012-08-30 00:31:07 -0500

    much greater forward progress

--- cards.pl
+++ cards.pl
@@ -3,50 +3,63 @@ use Image::Magick;
 use File::Path qw(make_path remove_tree);
 use 5.010;
 use Config::JSON;
+use utf8;
 
 ## settings
-my $out_path = '/tmp/game/';
 my $assets = '/data/Lacuna-Assets/';
 my $config = Config::JSON->new('game.conf');
 
 ## main
 init();
-generate_planet_cards();
+generate_decks();
 
 ## subs
 sub init {
-    remove_tree $out_path;
-    make_path $out_path;
+    remove_tree $config->get('out_path');
 }
 
-sub generate_planet_cards {
-    foreach my $card (@{$config->get('production')}) {
-        say $card->{name};
-        generate_planet_card($card);
+sub generate_decks {
+    foreach my $deck (@{$config->get('decks')}) {
+        next if $deck->{name} eq 'Production';
+        generate_deck($deck);
     }
 }
 
-sub generate_planet_card {
-    my ($attributes) = @_;
-    my $card = Image::Magick->new(size=>'600x825');
+sub generate_deck {
+    my $deck = shift;
+    my $out_path = $config->get('out_path').'/'.$deck->{name};
+        say $out_path;
+    make_path $out_path;
+    foreach my $card (@{$deck->{cards}}) {
+        say $card->{name};
+        generate_card($out_path, $deck, $card);
+    }
+}
+
+sub generate_card {
+    my ($out_path, $deck, $attributes) = @_;
+    my $card = Image::Magick->new(size=>$deck->{size});
     say $card->ReadImage('canvas:white');
     my $surface = Image::Magick->new;
-    say $surface->ReadImage($assets.'/planet_side/surface-p15.jpg');
+    say $surface->ReadImage($attributes->{background} || $deck->{background});
     say $surface->Rotate(90);
-    say $surface->Resize('600x825!');
+    say $surface->Resize($deck->{size}.'!');
     say $card->Composite(compose => 'over', image => $surface, x => 0, y => 0);
     #say $card->Draw(stroke=>'red', fill => 'none', strokewidth=>1, primitive=>'rectangle', points=>'38,38 562,787');
     say $card->Annotate(text => $attributes->{name}, font => 'ALIEN5.ttf', y => -275, fill => 'white', pointsize => 70, gravity => 'Center');
     my $image = Image::Magick->new;
-    say $image->ReadImage($assets.'/planet_side/400/'.$attributes->{image});
+    say $image->ReadImage($attributes->{image});
+    if ($attributes->{resize}) {
+        say $image->Resize('400x400');
+    }
     say $card->Composite(compose => 'over', image => $image, x => 100, y => 165);
     $card->Set(font => 'promethean.ttf', pointsize => 35);
     say $card->Annotate(text => wrap($attributes->{description}, $card, 400), x => 100, y => 600, font => 'promethean.ttf', fill => 'white', pointsize => 35);
-    say $card->Draw(stroke=>'black', fill => $attributes->{left} == 2 ? 'blue' : 'black', strokewidth=>1, primitive=>'polygon', points=>'65,400 65,450 40,425') if $attributes->{left};
-    say $card->Draw(stroke=>'black', fill => $attributes->{right} == 2 ? 'blue' : 'black', strokewidth=>1, primitive=>'polygon', points=>'535,400 535,450 560,425') if $attributes->{right};
-    say $card->Draw(stroke=>'black', fill => $attributes->{top} == 2 ? 'blue' : 'black', strokewidth=>1, primitive=>'polygon', points=>'275,65 325,65 300,40') if $attributes->{top};
-    say $card->Draw(stroke=>'black', fill => $attributes->{bottom} == 2 ? 'blue' : 'black', strokewidth=>1, primitive=>'polygon', points=>'275,760 325,760 300,785') if $attributes->{bottom};
-    $card->Write($out_path.$attributes->{name}.'.png');
+    say $card->Draw(stroke => $attributes->{left}, fill => $attributes->{left}, strokewidth=>1, primitive=>'polygon', points=>'65,400 65,450 40,425') if $attributes->{left};
+    say $card->Draw(stroke => $attributes->{right}, fill => $attributes->{right}, strokewidth=>1, primitive=>'polygon', points=>'535,400 535,450 560,425') if $attributes->{right};
+    say $card->Draw(stroke => $attributes->{top}, fill => $attributes->{top}, strokewidth=>1, primitive=>'polygon', points=>'275,65 325,65 300,40') if $attributes->{top};
+    say $card->Draw(stroke => $attributes->{bottom}, fill => $attributes->{bottom}, strokewidth=>1, primitive=>'polygon', points=>'275,760 325,760 300,785') if $attributes->{bottom};
+    $card->Write($out_path.'/'.$attributes->{name}.'.png');
 }
 
 # This function will wrap at a space or hyphen, and if a word is longer than a
