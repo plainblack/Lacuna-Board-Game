cc9e00b09f4ad1bbd048c30ab4aae7fa59163a2e
JT Smith <jt@plainblack.com>
2012-10-20 19:09:54 -0500

    now using TheGameCrafter::Client

--- upload.pl
+++ upload.pl
@@ -1,18 +1,25 @@
 use strict;
 use 5.010;
-use LWP::UserAgent;
-use HTTP::Request::Common;
-use JSON;
-use URI;
-use Data::Printer;
+use TheGameCrafter::Client;
 use DateTime;
 use Config::JSON;
+use Getopt::Long;
 
-my $config = Config::JSON->new('game.conf');
+# settings
+my $config_file;
+GetOptions(
+    'config=s' => \$config_file,
+);
+unless ($config_file) {
+    say "Usage: $0 --config=game.conf";
+    exit;
+}
+my $config = Config::JSON->new($config_file);
+
 
 # create session
 say "Creating session";
-my $session = post('session',[
+my $session = tgc_post('session',[
      username    => $ENV{TGC_USER},
      password    => $ENV{TGC_PASS},
      api_key_id  => $ENV{TGC_API_KEY},
@@ -20,7 +27,7 @@ my $session = post('session',[
 
 # get user
 say "Fetching user";
-my $user = get('user/'.$session->{user_id},[
+my $user = tgc_get('user/'.$session->{user_id},[
     session_id  => $session->{id},
     include_related_objects => 1,
     include_relationships => 1,
@@ -28,23 +35,23 @@ my $user = get('user/'.$session->{user_id},[
 
 # get game
 say "Fetching game details";
-my $game = get('game/'.$config->get('game_id'), [
+my $game = tgc_get('game/'.$config->get('game_id'), [
     session_id  => $session->{id},
     include_relationships => 1,
 ]);
 
 # delete old decks
 say "Deleting old decks";
-my $decks = get($game->{_relationships}{minidecks});
+my $decks = tgc_get($game->{_relationships}{minidecks});
 foreach my $deck (@{$decks->{items}}) {
-    del('minideck/'.$deck->{id}, [ 
+    tgc_delete('minideck/'.$deck->{id}, [ 
         session_id  => $session->{id},
     ]);
 }
 
 # create folder
 say "Creating game folder";
-my $game_folder = post('folder',[
+my $game_folder = tgc_post('folder',[
     name        => 'Lacuna Expanse',
     session_id  => $session->{id},
     user_id     => $user->{id},
@@ -56,7 +63,7 @@ say "Creating new decks";
 foreach my $deck_config (@{$config->get('decks')}) {
     say $deck_config->{name};
     say "Creating folder";
-    my $folder = post('folder',[
+    my $folder = tgc_post('folder',[
         name        => $deck_config->{name},
         session_id  => $session->{id},
         user_id     => $user->{id},
@@ -64,14 +71,14 @@ foreach my $deck_config (@{$config->get('decks')}) {
     ]);
     my $out_path = $config->get('out_path').'/'.$deck_config->{name};
     say "Uploading deck back";
-    my $back = post('file',[
+    my $back = tgc_post('file',[
         name        => $deck_config->{name}.'.png',
         folder_id   => $folder->{id},
         file        => [$out_path .'.png'],
         session_id  => $session->{id},
     ]);
     say "Creating deck";
-    my $deck = post('minideck', [
+    my $deck = tgc_post('minideck', [
         name                => $deck_config->{name},
         game_id             => $game->{id},
         session_id          => $session->{id},
@@ -82,14 +89,14 @@ foreach my $deck_config (@{$config->get('decks')}) {
     foreach my $card_config (@{$deck_config->{cards}}) {
         say $card_config->{name};
         say "Uploading face";
-        my $face = post('file',[
+        my $face = tgc_post('file',[
             name        => $card_config->{name},
             folder_id   => $folder->{id},
             file        => [$out_path .'/'. $card_config->{name}.'.png'],
             session_id  => $session->{id},
         ]);
         say "Creating card";
-        my $card = post('minicard', [
+        my $card = tgc_post('minicard', [
             name                => $card_config->{name},
             quantity            => $card_config->{quantity},
             deck_id             => $deck->{id},
@@ -101,57 +108,4 @@ foreach my $deck_config (@{$config->get('decks')}) {
 }
 
 say "All done!";
-exit;
-
-sub get {
-    my ($path, $params) = @_;
-    unless ($path =~ m/^\/api/) {
-        $path = '/api/'.$path;
-    }
-    my $uri = URI->new('https://www.thegamecrafter.com'.$path);
-    $uri->query_form($params);
-    my $response = LWP::UserAgent->new->request( GET $uri->as_string);
-    my $result = from_json($response->decoded_content); 
-    if ($response->is_success) {
-        say $result->{result}{object_name}.' ID: ', $result->{result}{id};
-        return $result->{result};
-    }
-    else {
-        die 'Error: ', $result->{error}{message};
-    }
-}
-
-sub del {
-    my ($path, $params) = @_;
-    unless ($path =~ m/^\/api/) {
-        $path = '/api/'.$path;
-    }
-    my $uri = URI->new('https://www.thegamecrafter.com'.$path);
-    $uri->query_form($params);
-    my $response = LWP::UserAgent->new->request( POST 'https://www.thegamecrafter.com'.$path, 'X-HTTP-Method' => 'DELETE', Content_Type => 'form-data', Content => $params );
-    my $result = from_json($response->decoded_content); 
-    if ($response->is_success) {
-        say "Deleted successfully!";
-    }
-    else {
-        die 'Error: ', $result->{error}{message};
-    }
-}
-
-sub post {
-    my ($path, $params) = @_;
-    unless ($path =~ m/^\/api/) {
-        $path = '/api/'.$path;
-    }
-    my $response = LWP::UserAgent->new->request( POST 'https://www.thegamecrafter.com'.$path, Content_Type => 'form-data', Content => $params );
-    my $result = from_json($response->decoded_content); 
-    if ($response->is_success) {
-        say $result->{result}{object_name}.' ID: ', $result->{result}{id};
-        sleep 3; # so as not to overuse my RPCs
-        return $result->{result};
-    }
-    else {
-        die 'Error: '. $response->status_line. ' '. $result->{error}{message};
-    }
-}
 
